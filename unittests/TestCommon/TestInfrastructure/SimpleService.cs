e312d357e55ecc705c0963d0c1dfb19441282ff3
Thanh Tran <hoangthanh28@gmail.com>
2023-12-10 02:40:16 -0500

    - add console log with default log level = none

---
+++ unittests/TestCommon/TestInfrastructure/SimpleService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TestCommon.TestInfrastructure;
+public class SimpleService
+{
+    public IServiceProvider ServiceProvider { get; }
+    public SimpleService()
+    {
+        var s_logLevel = Environment.GetEnvironmentVariable("Logging__LogLevel__Default");
+        ServiceProvider = new ServiceCollection()
+            .AddLogging(opt =>
+            {
+                opt.AddConsole();
+                if (Enum.TryParse(typeof(LogLevel), s_logLevel, out var logLevel))
+                {
+                    opt.SetMinimumLevel((LogLevel)logLevel);
+                }
+                else
+                {
+                    opt.SetMinimumLevel(LogLevel.None);
+                }
+            })
+            .BuildServiceProvider();
+    }
+}
